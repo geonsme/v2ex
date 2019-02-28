52235cc6c7e298c0036329a58a568590f0cf5cef
Geons <1090700965@qq.com>
2019-02-28 11:09:22 +0800

    新建weex工程

---
+++ v2ex-app/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <text class="message">This is my first webapp, only for my DouDou . </text>
+</template>
+
+<style scoped>
+
+  .message {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 30px;
+    color: rgb(245, 177, 52);
+  }
+
+</style>
